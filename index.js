32c75dedbb46ef69a301aadbd5cd3d8cec605e56
IanVitor <76691828+IanVitor@users.noreply.github.com>
2023-06-26 17:24:16 -0300

    feature
    add image to unknow user's

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+const path = require('path');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    socket.broadcast.emit('chat message', msg);
+  socket.on('chat message', (msg, name, img) => {
+    socket.broadcast.emit('chat message', msg, name, img);
   });
 });
 
